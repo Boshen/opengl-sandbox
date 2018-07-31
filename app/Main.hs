d0028aba1d0e54c5ba1c4fbec2a3c295e7aa2cb3
Boshen <boshenc@gmail.com>
2018-07-31 19:46:57 +1200

    draw a triangle

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE CPP               #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
@@ -7,6 +6,9 @@ import           Control.Monad
 import qualified Data.ByteString           as BS
 import qualified Data.Vector.Storable      as V
 import           Foreign.C.Types
+import           Foreign.Marshal.Array
+import           Foreign.Ptr
+import           Foreign.Storable
 import           SDL.Vect
 import           System.Exit               (exitFailure)
 import           System.IO
@@ -14,9 +16,27 @@ import           System.IO
 import qualified Graphics.Rendering.OpenGL as GL
 import           SDL                       (($=))
 import qualified SDL
+import           SDL.Video.OpenGL          (Mode (Normal))
+
+import           LoadShaders
+
+data Descriptor =
+  Descriptor GL.VertexArrayObject
+             GL.ArrayIndex
+             GL.NumArrayIndices
 
 screenWidth, screenHeight :: CInt
 (screenWidth, screenHeight) = (640, 480)
+
+openGLConfig :: SDL.OpenGLConfig
+openGLConfig =
+  SDL.OpenGLConfig
+    { SDL.glColorPrecision = V4 8 8 8 0
+    , SDL.glDepthPrecision = 24
+    , SDL.glStencilPrecision = 8
+    , SDL.glMultisampleSamples = 1
+    , SDL.glProfile = SDL.Core Normal 4 1
+    }
 
 main :: IO ()
 main = do
@@ -27,95 +47,70 @@ main = do
        putStrLn "Warning: Linear texture filtering not enabled!"
   window <-
     SDL.createWindow
-      "SDL / OpenGL Example"
+      "SDL + OpenGL"
       SDL.defaultWindow
         { SDL.windowInitialSize = V2 screenWidth screenHeight
-        , SDL.windowOpenGL = Just SDL.defaultOpenGL
+        , SDL.windowOpenGL = Just openGLConfig
         }
   SDL.showWindow window
-  _ <- SDL.glCreateContext window
-  (prog, attrib) <- initResources
-  let loop = do
-        events <- SDL.pollEvents
-        let quit = elem SDL.QuitEvent $ map SDL.eventPayload events
-        GL.clear [GL.ColorBuffer]
-        draw prog attrib
-        SDL.glSwapWindow window
-        unless quit loop
-  loop
+  SDL.glCreateContext window
+  discriptor <- initResources
+  onDisplay window discriptor
   SDL.destroyWindow window
   SDL.quit
 
-initResources :: IO (GL.Program, GL.AttribLocation)
-initResources
-  -- compile vertex shader
- = do
-  vs <- GL.createShader GL.VertexShader
-  GL.shaderSourceBS vs $= vsSource
-  GL.compileShader vs
-  vsOK <- GL.get $ GL.compileStatus vs
-  unless vsOK $ do
-    hPutStrLn stderr "Error in vertex shader\n"
-    exitFailure
-    -- Do it again for the fragment shader
-  fs <- GL.createShader GL.FragmentShader
-  GL.shaderSourceBS fs $= fsSource
-  GL.compileShader fs
-  fsOK <- GL.get $ GL.compileStatus fs
-  unless fsOK $ do
-    hPutStrLn stderr "Error in fragment shader\n"
-    exitFailure
-  program <- GL.createProgram
-  GL.attachShader program vs
-  GL.attachShader program fs
-  GL.attribLocation program "coord2d" $= GL.AttribLocation 0
-  GL.linkProgram program
-  linkOK <- GL.get $ GL.linkStatus program
-  GL.validateProgram program
-  status <- GL.get $ GL.validateStatus program
-  unless (linkOK && status) $ do
-    hPutStrLn stderr "GL.linkProgram error"
-    plog <- GL.get $ GL.programInfoLog program
-    putStrLn plog
-    exitFailure
-  GL.currentProgram $= Just program
-  return (program, GL.AttribLocation 0)
-
-draw :: GL.Program -> GL.AttribLocation -> IO ()
-draw program attrib = do
+onDisplay :: SDL.Window -> Descriptor -> IO ()
+onDisplay window descriptor = do
   GL.clearColor $= GL.Color4 1 1 1 1
   GL.clear [GL.ColorBuffer]
   GL.viewport $=
     ( GL.Position 0 0
     , GL.Size (fromIntegral screenWidth) (fromIntegral screenHeight))
+  draw descriptor
+  SDL.glSwapWindow window
+  events <- SDL.pollEvents
+  let quit = elem SDL.QuitEvent $ map SDL.eventPayload events
+  unless quit (onDisplay window descriptor)
+
+bufferOffset :: Integral a => a -> Ptr b
+bufferOffset = plusPtr nullPtr . fromIntegral
+
+initResources :: IO Descriptor
+initResources = do
+  triangles <- GL.genObjectName
+  GL.bindVertexArrayObject $= Just triangles
+  arrayBuffer <- GL.genObjectName
+  GL.bindBuffer GL.ArrayBuffer $= Just arrayBuffer
+  withArray vertices $ \ptr -> do
+    let size = fromIntegral (numVertices * sizeOf (head vertices))
+    GL.bufferData GL.ArrayBuffer $= (size, ptr, GL.StaticDraw)
+  program <-
+    loadShaders
+      [ ShaderInfo GL.VertexShader (FileSource "./app/shader.vert")
+      , ShaderInfo GL.FragmentShader (FileSource "./app/shader.frag")
+      ]
   GL.currentProgram $= Just program
-  GL.vertexAttribArray attrib $= GL.Enabled
-  V.unsafeWith vertices $ \ptr ->
-    GL.vertexAttribPointer attrib $=
-    (GL.ToFloat, GL.VertexArrayDescriptor 2 GL.Float 0 ptr)
-  GL.drawArrays GL.Triangles 0 3 -- 3 is the number of vertices
-  GL.vertexAttribArray attrib $= GL.Disabled
+  let firstIndex = 0
+      vPosition = GL.AttribLocation 0
+  GL.vertexAttribPointer vPosition $=
+    ( GL.ToFloat
+    , GL.VertexArrayDescriptor 2 GL.Float 0 (bufferOffset firstIndex))
+  GL.vertexAttribArray vPosition $= GL.Enabled
+  return $ Descriptor triangles firstIndex (fromIntegral numVertices)
 
-vsSource, fsSource :: BS.ByteString
-vsSource =
-  BS.intercalate
-    "\n"
-    [ "attribute vec2 coord2d; "
-    , ""
-    , "void main(void) { "
-    , " gl_Position = vec4(coord2d, 0.0, 1.0); "
-    , "}"
-    ]
+draw :: Descriptor -> IO ()
+draw (Descriptor triangles firstIndex numVertices) = do
+  GL.bindVertexArrayObject $= Just triangles
+  GL.drawArrays GL.Triangles firstIndex numVertices
 
-fsSource =
-  BS.intercalate
-    "\n"
-    [ ""
-    , "#version 120"
-    , "void main(void) {"
-    , "gl_FragColor = vec4((gl_FragCoord.x/640), (gl_FragCoord.y/480), 0, 1);"
-    , "}"
-    ]
+vertices :: [GL.Vertex2 GL.GLfloat]
+vertices =
+  [ GL.Vertex2 (-0.90) (-0.90) -- Triangle 1
+  , GL.Vertex2 0.85 (-0.90)
+  , GL.Vertex2 (-0.90) 0.85
+  -- , GL.Vertex2 0.90 (-0.85) -- Triangle 2
+  -- , GL.Vertex2 0.90 0.90
+  -- , GL.Vertex2 (-0.85) 0.90
+  ]
 
-vertices :: V.Vector Float
-vertices = V.fromList [0.0, 0.8, -0.8, -0.8, 0.8, -0.8]
+numVertices = length vertices
---
+++ app/shader.frag
@@ -0,0 +1,8 @@
+#version 410 core
+
+out vec4 fColor;
+void
+main()
+{
+   fColor = vec4(0.0, 0.5, 1.0, 1.0);
+}
